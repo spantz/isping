8774542d06c5d10f6fa884b036afc2bccbc32b11
James Spence <Jamesaspence20@gmail.com>
2017-07-22 15:56:39 -0500

    test script

--- bin/test.js
+++ bin/test.js
@@ -1,4 +1,10 @@
 const SpeedTest = require('./../lib/SpeedTest.js');
+const tokenRepository = require('./../lib/TokenRepository');
+
+if (!tokenRepository.tokenSaved()) {
+    console.error('No token saved!');
+    return;
+}
 
 let test = new SpeedTest();
 
